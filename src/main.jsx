d02669298e7c0eccab108d5852f6851605c505da
kesavan <kesavankishore3@gmail.com>
2023-12-01 15:41:54 +0530

    changes can updated in app to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import "./App.css";
+import Create from "./Components/Create"
+import Read from "./Components/Read"
+import Update from "./Components/Update"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+  <div className="main">
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/create" element={<Create />} />
+      <Route exact path="/read" element={ <Read />} />
+      <Route exact path="update" element={<Update />} />
+    </Routes>
+  </BrowserRouter>
+</div>
   
 )
