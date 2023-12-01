b84483c0a74b74d3133c5f2f87c68f1bc79cca91
kesavan <kesavankishore3@gmail.com>
2023-12-01 15:58:59 +0530

    the changes is created button in main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,37 @@
 
+import "./App.css";
+import Create from "./Components/Create"
+import Read from "./Components/Read"
+import Update from "./Components/Update"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { useNavigate} from "react"
+import { Button } from 'semantic-ui-react'
+
+
+function App() {
+
+
+  const createpage = () => {
+    navigate("/create")
+  }
+
+  const navigate = useNavigate()
+  return (
+    <div className="main">
+      <h1>CRUD OPERATION</h1>
+     <Button onClick={createpage}>Please Click To Go <h3>CRUD</h3> Operation Page</Button>
+      
+
+      
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/create" element={<Create />} />
+          <Route exact path="/read" element={ <Read />} />
+          <Route exact path="update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 
 import ReactDOM from 'react-dom/client'
-import "./App.css";
-import Create from "./Components/Create"
-import Read from "./Components/Read"
-import Update from "./Components/Update"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import App from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-  <div className="main">
-  <BrowserRouter>
-    <Routes>
-      <Route exact path="/create" element={<Create />} />
-      <Route exact path="/read" element={ <Read />} />
-      <Route exact path="update" element={<Update />} />
-    </Routes>
-  </BrowserRouter>
-</div>
+    <App />
   
 )
