81adfd9fe1bed7a7dc6ed5b024b3b1caa31559fd
kesavan <kesavankishore3@gmail.com>
2023-12-01 16:07:38 +0530

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,18 @@ import Create from "./Components/Create"
 import Read from "./Components/Read"
 import Update from "./Components/Update"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import { useNavigate} from "react"
-import { Button } from 'semantic-ui-react'
+
 
 
 function App() {
 
 
-  const createpage = () => {
-    navigate("/create")
-  }
 
-  const navigate = useNavigate()
+ 
   return (
     <div className="main">
       <h1>CRUD OPERATION</h1>
-     <Button onClick={createpage}>Please Click To Go <h3>CRUD</h3> Operation Page</Button>
+     
       
 
       
